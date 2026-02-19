b82ef8cd6a91f58ff87283cceb2bca92d4cbe69b
kolren <maichol04aprea18@gmail.com>
2026-02-19 10:37:31 +0100

    Change password placeholder to 'inserisci password'
    Updated the password input placeholder text.

--- front-page.php
+++ front-page.php
@@ -154,7 +154,7 @@ $_SESSION['captcha_verified'] = false;
 
             <div class="input-group mb-5 relative">
                 <label>Password</label>
-                <input type="password" name="password" id="password" class="glass-input" placeholder="••••••••" required>
+                <input type="password" name="password" id="password" class="glass-input" placeholder="inserisci password" required>
                 <div class="toggle-password" onclick="togglePass()">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
                 </div>
